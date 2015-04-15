4d9d3d874e5c2a7c698ab5ba282a0517721f4243
nao-pon <hypweb@gmail.com>
2015-04-16 00:49:29 +0900

    whatsnew version 2.61

--- html/modules/whatsnew/include/whatsnew_version.php
+++ html/modules/whatsnew/include/whatsnew_version.php
@@ -17,8 +17,8 @@
 // --- define version begin ---
 if( !defined('_WHATSNEW_VERSION') ) 
 {
-	define('_WHATSNEW_VERSION', '2.60');
-	define('_WHATSNEW_HAPPY_LINUX_VERSION', '1.60');
+	define('_WHATSNEW_VERSION', '2.61');
+	define('_WHATSNEW_HAPPY_LINUX_VERSION', '1.82');
 }
 // --- define version end ---
 
